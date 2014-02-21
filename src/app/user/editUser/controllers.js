4b796fafbb74f6305764bfb090c03fd67cc94c15
pgonzalezAlivebox <pgonzalez@alivebox.com>
2014-02-21 16:11:07 -0600

    basic edit user

--- src/app/user/editUser/controllers.js
+++ src/app/user/editUser/controllers.js
@@ -5,16 +5,41 @@ define(['angular', './module'], function (ng, module) {
     /**
      * And of course we define a controller for our route.
      */
-    module.controller( 'UserCtrl', function UserController( $scope, $http ) {
+    module.controller( 'UserCtrl', function UserController( $scope, $http, $location ) {
         console.log("UserCtrl init....");
 
-        if($scope.id !== undefined){
+        $scope.user = {name: "", id:0};
+
+        $scope.init = function(){
+            console.log("init function .....");
+            if(this.$id){
+                this.loadEditMode();
+                return;
+            }
+            this.loadCreateMode();
+        };
+
+        $scope.loadCreateMode= function(){
+
+        };
+
+        $scope.loadEditMode = function(){
             this.loadUser();
-        }
+        };
 
         $scope.loadUser = function(){
             console.log("loading user ....");
-
+            $http({
+                url: 'http://jsonstub.com/user/1',
+                method: 'GET',
+                headers: {
+                    'JsonStub-User-Key': 'fdc9e8dc-e2db-49ec-be37-b915ad00e98b',
+                    'JsonStub-Project-Key': 'd71daa77-1ca3-411a-8d60-a5f5201f932b'
+                }
+            }).success(function (data, status, headers, config) {
+                console.log("user loaded ...");
+                $scope.user = data;
+            });
         };
 
         $scope.saveUser = function(argUser){
@@ -30,6 +55,7 @@ define(['angular', './module'], function (ng, module) {
             }).success(function (data, status, headers, config) {
                     //do something with data
                     console.log("Success save ....");
+                    $location.path("/listUsers");
             });
         };
     });
--- src/app/user/listUsers/controllers.js
+++ src/app/user/listUsers/controllers.js
@@ -13,20 +13,27 @@ define(['angular', './module'], function (ng, module) {
      */
     module.controller( 'ListUsersCtrl', function ListUsersController( $scope, $http ) {
         console.log("ListCtrl init....");
-        $http({
-            url: 'http://jsonstub.com/users',
-            method: 'GET',
-            headers: {
-                'JsonStub-User-Key': 'fdc9e8dc-e2db-49ec-be37-b915ad00e98b',
-                'JsonStub-Project-Key': 'd71daa77-1ca3-411a-8d60-a5f5201f932b'
-            }
-        }).success(function (data, status, headers, config) {
-            //do something with data
-            console.log("Success GET ListCtrl");
-            $scope.users = data.users;
-        }).error(function (data, status, headers, config) {
-            console.log("Error GET ListCtrl");
-        });
+
+        $scope.init = function(){
+            this.loadUsers();
+        };
+
+        $scope.loadUsers = function(){
+            $http({
+                url: 'http://jsonstub.com/users',
+                method: 'GET',
+                headers: {
+                    'JsonStub-User-Key': 'fdc9e8dc-e2db-49ec-be37-b915ad00e98b',
+                    'JsonStub-Project-Key': 'd71daa77-1ca3-411a-8d60-a5f5201f932b'
+                }
+            }).success(function (data, status, headers, config) {
+                    //do something with data
+                    console.log("Success GET ListCtrl");
+                    $scope.users = data.users;
+                }).error(function (data, status, headers, config) {
+                    console.log("Error GET ListCtrl");
+                });
+        };
     });
 
 });
