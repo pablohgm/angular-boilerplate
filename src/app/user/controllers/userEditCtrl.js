e9d3a74f1cb1c8a503310da327c7bf206144ab6c
pgonzalezAlivebox <pgonzalez@alivebox.com>
2014-02-26 22:15:19 -0600

    save and edit implementation

--- src/app/user/controllers/userEditCtrl.js
+++ src/app/user/controllers/userEditCtrl.js
@@ -8,18 +8,18 @@ define(['angular', './module'], function (ng, module) {
     module.controller( 'UserEditCtrl', function UserEditController( $scope, $http, $location, $stateParams, storage, LocalStorageNotificationSrv ) {
         console.log("UserEditCtrl init....");
 
-        $scope.user = {name: "", id:0, birthDate: null};
+        $scope.user = {name: '', id:'', birthDate: null, children:''};
         $scope.alerts = [];
         $scope.userList = [];
 
         $scope.init = function(){
             console.log("init function .....");
+            this.loadUserList();
             if(!_.isUndefined($stateParams.userId) ){
                 this.loadEditMode();
                 return;
             }
             this.loadCreateMode();
-            this.loadUserList();
         };
 
         $scope.loadUserList = function(){
@@ -39,21 +39,38 @@ define(['angular', './module'], function (ng, module) {
 
         $scope.loadUser = function(){
             console.log("loading user ....");
-            $http({
-                url: 'http://jsonstub.com/user/1',
-                method: 'GET',
-                headers: {
-                    'JsonStub-User-Key': 'fdc9e8dc-e2db-49ec-be37-b915ad00e98b',
-                    'JsonStub-Project-Key': 'd71daa77-1ca3-411a-8d60-a5f5201f932b'
-                }
-            }).success(function (data, status, headers, config) {
-                console.log("user loaded ...");
-                $scope.user = data;
+            var tmpId = $stateParams.userId;
+            var tmpUser = _.find($scope.userList, function(argUser) {
+                return argUser.id === tmpId;
             });
+            if(_.isUndefined(tmpUser)){
+                return;
+            }
+            $scope.user = tmpUser;
         };
 
         $scope.saveUser = function(argUser){
             console.log("Saving ....");
+            if(!_.isUndefined($stateParams.userId) ){
+                this.editUser(argUser);
+                return;
+            }
+            this.createUser(argUser);
+        };
+
+        $scope.editUser = function(argUser){
+            var tmpId = $stateParams.userId;
+            var tmpIndex = _.findIndex($scope.userList, function(argUser) {
+                return argUser.id === tmpId;
+            });
+            $scope.userList.splice(tmpIndex, 1);
+            $scope.userList.push(argUser);
+            storage.set("userList", this.userList);
+            var tmpNotification = { type: 'success', msg: 'Well done! The user has been saved' };
+            LocalStorageNotificationSrv.setSuccessNotification(tmpNotification);
+        };
+
+        $scope.createUser = function(argUser){
             $scope.userList.push(argUser);
             storage.set("userList", this.userList);
             var tmpNotification = { type: 'success', msg: 'Well done! The user has been saved' };
